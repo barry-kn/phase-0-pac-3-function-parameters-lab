56ac7d19fa8abc1124d4a144f07e5e9023c28117
barry-kn <carrybarryklein49@gmail.com>
2023-03-08 16:43:55 +0300

    complete Asignment

---
+++ index.js
@@ -0,0 +1,31 @@
+ 
+function introduction(name){
+    console.log(`Hi,my name is ${name}.`);
+}
+function introductionWithLanguage(name, language){
+    console.log(`Hi, my name is ${name} and i am learning to code ${language}.`);
+}
+function helloToSay(name, language = "javas"){
+    console.log(`hello, my name is ${name} and i am learning to codein ${language}`);
+}
+introduction("barry")
+introductionWithLanguage("barry", "javas")
+helloToSay("barry")
+
+
+function introduction(name){
+    console.log(`Hi,my name is ${name}.`);
+}
+function introductionWithLanguage(name, language){
+    console.log(`Hi, my name is ${name} and i am learning to code ${language}.`);
+}
+function helloToSay(name, language = "javas"){
+    console.log(`hello, my name is ${name} and i am learning to codein ${language}`);
+}
+introduction("barry")
+introductionWithLanguage("barry", "javas")
+helloToSay("barry")
+
+
+
+
